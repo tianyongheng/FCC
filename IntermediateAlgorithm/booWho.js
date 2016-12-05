b95a5a36b8a3902587bea9897c8b685614713b3f
tianyongheng <790702854@qq.com>
2016-12-05 13:48:21 +0800

    检查一个值是否是基本布尔类型

---
+++ IntermediateAlgorithm/booWho.js
@@ -0,0 +1,10 @@
+function boo(bool) {
+  if(bool===true||bool===false){
+  // What is the new fad diet for ghost developers? The Boolean.
+  return true;
+  }else{
+    return false;
+  }
+}
+
+boo(null);
