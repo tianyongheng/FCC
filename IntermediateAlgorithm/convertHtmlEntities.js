ea52b27d12502ff0c1491eadc9a454532d2e0f2a
tianyongheng <790702854@qq.com>
2016-12-05 15:25:57 +0800

    将\&,\<,\>,"以及\'转化为对应的html实体

---
+++ IntermediateAlgorithm/convertHtmlEntities.js
@@ -0,0 +1,11 @@
+function convert(str) {
+  // &colon;&rpar;
+  str = str.replace(/\&/g,"&amp;");
+  str = str.replace(/\>/g,"&gt;");
+  str = str.replace(/\</g,"&lt;");
+  str = str.replace(/\"/g,"&quot;");
+  str = str.replace(/\'/g,"&apos;");
+  return str;
+}
+
+convert("Dolce & Gabbana");
