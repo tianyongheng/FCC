d99e024b5bfb0a411012c240c951cdf557d61038
tianyongheng <790702854@qq.com>
2016-12-10 18:06:40 +0800

    找零

--- IntermediateAlgorithm/ExactChange.js
+++ IntermediateAlgorithm/ExactChange.js
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	<script>
 function checkCashRegister(price, cash, cid) {
   price = price*100;
   cash = cash*100;
